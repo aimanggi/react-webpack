88f43c515b5f1b9249ba8a29ff682f13def4e312
Anggi Maulinda <anggi.maulinda@webimp.com.sg>
2019-09-18 14:23:14 +0800

    install path

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,38 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    entry: {
-        app: './src/App.js',
+  entry: {
+    app: './src/App.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       },
-      output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-      },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/index.html',
-            filename: './index.html',
-          }),
-    ],
-    module: {
-        rules: [
+      {
+        test: /\.html$/,
+        use: [
           {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
-            }
-          },
-          {
-            test: /\.html$/,
-            use: [
-              {
-                loader: "html-loader"
-              }
-            ]
+            loader: "html-loader"
           }
         ]
-    
-}
+      }
+    ]
+
+  }
 }
